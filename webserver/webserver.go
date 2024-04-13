a0eff54ec67c42f8af109066613466c0781e3c3b
Leon Latsch <git@leonlatsch.dev>
2024-04-13 12:49:07 +0200

    remove gin gonic

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -3,14 +3,39 @@ package webserver
 import (
 	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"github.com/leonlatsch/fred/jsinject"
 )
 
 func SetupWebServer() {
-	ginRouter := gin.New()
-	ginRouter.Static("", ".")
 	log.Println("fred now serving at http://localhost:8000")
-	http.ListenAndServe(":8000", &jsinject.WsInjectorMiddleware{Next: ginRouter})
+	http.ListenAndServe(":8000", &HotReloadFileServer{})
 }
+
+type HotReloadFileServer struct{}
+
+func (handler *HotReloadFileServer) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte(clientWebsocketCode))
+	fs := http.FileServer(http.Dir("."))
+	fs.ServeHTTP(writer, req)
+}
+
+const clientWebsocketCode = `
+<script>
+(() => {
+    const createSocket = () => {
+        const socketUrl = 'ws://localhost:8090';
+        let socket = new WebSocket(socketUrl);
+
+        socket.onmessage = (e) => {
+            console.log(e)
+            location.reload()
+        }
+
+        socket.onclose = () => {
+            setTimeout(createSocket, 1000)
+        }
+    }
+
+    createSocket()
+})();
+</script>
+`
